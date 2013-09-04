60ad9a261c13f8ef607d7e2c9e8930dd0e2fc7ec
Jesse Waites <jesse.waites@gmail.com>
2013-09-03 19:59:55 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thesocialnetwork::Application.config.secret_key_base = 'bfdf73d8ab35e2388df586b22cdb7e507d6c3076de980f21e4001a5047ce2ea74ea6378529779a60d644fae3cbc1410de715a15cf6215c743783e8ffc6afd2eb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Thesocialnetwork::Application.config.session_store :cookie_store, key: '_thesocialnetwork_session'
